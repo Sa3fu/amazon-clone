433bc8132229718895553745184f1fafe2a42d00
Sa3fu <saifuden786@gmail.com>
2024-02-28 11:07:37 +0530

    started adding product image price etc..

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     )
   );
   return (
-    <div className="font-bodyFont">
+    <div className="font-bodyFont bg-gray-100">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,10 +1,28 @@
-import React, { useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 
 function Products() {
   const data = useLoaderData();
   const produtData = data.data;
-  return <div>Products</div>;
+  return (
+    <div className="max-width-screen-2xl mx-auto grid grid-cols-4 gap-10 px-4">
+      {produtData.map((item) => (
+        <div className="bg-white h-auto border-[1px] border-gray-200 py-6 z-30 hover:border-transparent
+        shadow-none hover:shadow-testShadow relative">
+          <div className="w-full h-auto flex items-center justify-center">
+            <img
+              className="w-52 h-64 object-contain"
+              src={item.image}
+              alt="Product image"
+            />
+          </div>
+          <div>
+            <h2>{item.title}</h2>
+            <p>{item.price}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Products;
