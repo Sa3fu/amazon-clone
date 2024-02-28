f63cf3f61afe59956b447fdf54ca3d9f85452c21
Sa3fu <saifuden786@gmail.com>
2024-02-28 12:38:43 +0530

    add title and description to product card

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -6,8 +6,11 @@ function Products() {
   return (
     <div className="max-width-screen-2xl mx-auto grid grid-cols-4 gap-10 px-4">
       {produtData.map((item) => (
-        <div className="bg-white h-auto border-[1px] border-gray-200 py-6 z-30 hover:border-transparent
-        shadow-none hover:shadow-testShadow relative">
+        <div
+          key={item.id}
+          className="bg-white h-auto border-[1px] border-gray-200 py-6 z-30 hover:border-transparent
+        shadow-none hover:shadow-testShadow flex flex-col gap-4 relative"
+        >
           <div className="w-full h-auto flex items-center justify-center">
             <img
               className="w-52 h-64 object-contain"
@@ -15,9 +18,21 @@ function Products() {
               alt="Product image"
             />
           </div>
-          <div>
-            <h2>{item.title}</h2>
-            <p>{item.price}</p>
+          <div className="px-4">
+            <div className="flex items-center justify-between">
+              <h2
+                className=" font-titleFont tracking-wide text-lg text-amazon_blue 
+              font-medium"
+              >
+                {item.title.substring(0, 20)}
+              </h2>
+              <p className="text-sm text-gray-600 font-semibold">
+                ₹{item.price}
+              </p>
+            </div>
+            <div>
+              <p className="line-clamp-3 text-sm">{item.description}</p>
+            </div>
           </div>
         </div>
       ))}
